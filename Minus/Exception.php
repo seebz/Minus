375ae66797dd1065344e37fb28f3de87999bcd4e
Seebz <sebastien@seebz.net>
2013-03-22 15:14:11 +0100

    Ajout de la classe 'Exception'

---
+++ Minus/Exception.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Minus;
+
+
+/**
+ * Exception
+ */
+class Exception extends \Exception
+{
+
+	const ROUTE_NOT_FOUND      = 1;
+	const CONTROLLER_NOT_FOUND = 2;
+	const ACTION_NOT_FOUND     = 3;
+
+}
